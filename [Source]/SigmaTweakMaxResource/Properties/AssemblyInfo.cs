3e1792023fd424d76c38d8561d34d24f9d46e633
Sigma88 <sigmamodexpansions@outlook.com>
2018-03-21 18:36:22 +0100

    first release

---
+++ [Source]/SigmaTweakMaxResource/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SigmaTweakMaxResource")]
+[assembly: AssemblyDescription("Sigma Tweak MaxResource")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sigma88")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright (C) Sigma88 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: KSPAssembly("SigmaTweakMaxResource", 1, 0)]
+[assembly: AssemblyVersion("1.0.0")]
---
+++ [Source]/SigmaTweakMaxResource/SigmaTweakMaxResource.cs
@@ -0,0 +1,54 @@
+﻿namespace SigmaTweakMaxResource
+{
+    internal class ModuleTweakMaxResource : PartModule
+    {
+        [KSPField]
+        public string resourceName = "";
+
+        [KSPField(isPersistant = true)]
+        public bool tweakMaxAmount = false;
+
+        [KSPField(isPersistant = true)]
+        public double originalMaxAmount = 0;
+
+        public override void OnSave(ConfigNode node)
+        {
+            tweakMaxAmount = HighLogic.LoadedScene == GameScenes.EDITOR;
+            base.OnSave(node);
+        }
+
+        public override void OnInitialize()
+        {
+            if (HighLogic.LoadedScene == GameScenes.EDITOR && originalMaxAmount > 0)
+            {
+                for (int i = 0; i < part.Resources.Count; i++)
+                {
+                    var resource = part.Resources[i];
+
+                    if (resource.resourceName == resourceName)
+                    {
+                        resource.maxAmount = originalMaxAmount;
+                    }
+                }
+            }
+
+            if (HighLogic.LoadedScene == GameScenes.FLIGHT && tweakMaxAmount)
+            {
+                tweakMaxAmount = false;
+
+                for (int i = 0; i < part.Resources.Count; i++)
+                {
+                    var resource = part.Resources[i];
+
+                    if (resource.resourceName == resourceName)
+                    {
+                        if (originalMaxAmount == 0)
+                            originalMaxAmount = resource.maxAmount;
+
+                        resource.maxAmount = resource.amount;
+                    }
+                }
+            }
+        }
+    }
+}
